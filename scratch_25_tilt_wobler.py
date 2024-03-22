e913a1caf71cfa84d566b2084b2806ddf810ccbf
louim-lbs <louis-marie.lebas@insa-lyon.fr>
2024-03-22 16:23:30 +0100

    minor corr

--- scratch_25_tilt_wobler.py
+++ scratch_25_tilt_wobler.py
@@ -4,22 +4,23 @@ import sys
 from autoscript_sdb_microscope_client.structures import GrabFrameSettings
 from autoscript_sdb_microscope_client.enumerations import ScanningResolution
 
-sys.path.insert(1, r'D:\SharedData\LM LEBAS\Process_Integration')
-os.chdir(r'D:\SharedData\LM LEBAS\Process_Integration')
+actual_path = os.getcwd()
+sys.path.insert(1, actual_path)
+os.chdir(actual_path)
 
 import com_functions2
 
 dir_pi = os.getcwd()
 
-microscope = com_functions2.microscope().f
-microscope.import_package_and_connexion()
+# microscope = com_functions2.microscope().f
+# microscope.import_package_and_connexion()
 
 positioner = com_functions2.microscope().p
 positioner.import_package_and_connexion()
 
 os.chdir(dir_pi)
 
-alpha = 40
+alpha = 10
 
 pos = positioner.current_position()
 print(pos)
@@ -27,7 +28,7 @@ positioner.absolute_move(y=pos[1] , z=pos[2], a=0)
 time.sleep(0.5)
 positioner.relative_move(da = alpha)
 
-microscope.start_acquisition()
+# microscope.start_acquisition()
 while True:
     positioner.relative_move(da = -2*alpha)
     time.sleep(0.5)
